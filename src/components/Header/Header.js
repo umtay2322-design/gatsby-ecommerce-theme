fda6c310706ee371df294f581b7cd2a42101a954
umtay2322-design <umtay2322@gmail.com>
2026-01-03 14:56:57 +0300

    Değişiklik açıklaması

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,6 +14,7 @@ import Icon from '../Icons/Icon';
 import MiniCart from '../MiniCart';
 import MobileNavigation from '../MobileNavigation';
 import * as styles from './Header.module.css';
+import { FaWhatsapp } from "react-icons/fa";
 
 const Header = (prop) => {
   const [showMiniCart, setShowMiniCart] = useState(false);
